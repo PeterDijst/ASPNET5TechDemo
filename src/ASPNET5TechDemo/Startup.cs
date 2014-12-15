e555d8953f5777e3f0e1c327bd878fb085364de0
Jaap Hutink <JaapHutink@Outlook.com>
2014-12-15 18:26:19 +0100

    First MVC controller and view etc.

--- src/ASPNET5TechDemo/Startup.cs
+++ src/ASPNET5TechDemo/Startup.cs
@@ -2,19 +2,30 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Routing;
 
 namespace ASPNET5TechDemo
 {
     public class Startup
     {
-        public void RegisterServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddEntityFramework();
+            services.AddMvc();
         }
-
         public void Configure(IApplicationBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-                    }
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+        }
     }
 }
